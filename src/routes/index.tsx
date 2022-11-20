ba70f7ae53c7b0b5d2147c8130dcf8241bc64ac8
Alexander Davis <alexander.davis.098@gmail.com>
2022-11-20 11:06:15 -0300

    feat: Added light theme provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/initial-page" element={<p>Initial Page</p>} />
+            <Route path="/initial-page" element={<Button variant="contained" color="primary">Button</Button>} />
             <Route path="*" element={<Navigate to="/initial-page" />} />
         </Routes>
     );
