6e32f9e933c0184351651dc1631b0f04280f5912
Alexander Davis <alexander.davis.098@gmail.com>
2022-11-20 10:20:14 -0300

    feat: Added react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {  };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/initial-page" element={<p>Initial Page</p>} />
+            <Route path="*" element={<Navigate to="/initial-page" />} />
+        </Routes>
+    );
+}
