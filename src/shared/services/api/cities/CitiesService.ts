59c0cd5c1cb1f02a2c5350a219ba95075199a41c
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-01 12:21:19 -0300

    feat: Added cities service

---
+++ src/shared/services/api/cities/CitiesService.ts
@@ -0,0 +1,81 @@
+import { Environment } from '../../../environment';
+import { Api } from '../axios';
+
+export interface ICitiesListing {
+  id: number;
+  name: string;
+}
+
+export interface ICitiesDetails {
+  id: number;
+  name: string;
+}
+
+type TCitiesWithTotalCount = {
+  data: ICitiesListing[];
+  totalCount: number;
+}
+
+const getAll = async (page = 1, filter = ''): Promise<TCitiesWithTotalCount | Error> => {
+  try {
+    const relativeUrl = `/cities?_page=${page}&_limit=${Environment.LINE_LIMIT}&name_like=${filter}`;
+    const { data, headers } = await Api.get(relativeUrl);
+
+    if (data)
+      return { data, totalCount: Number(headers['x-total-count'] || Environment.LINE_LIMIT) };
+
+    return new Error('Error to listing records.');
+  } catch (error) {
+    return new Error((error as { message: string }).message || 'Error to listing records.');
+  }
+};
+
+const getById = async (id: number): Promise<ICitiesDetails | Error> => {
+  try {
+    const { data } = await Api.get(`/cities/${id}`);
+
+    if (data)
+      return data;
+
+    return new Error('Error querying a record.');
+  } catch (error) {
+    return new Error((error as { message: string }).message || 'Error querying a record.');
+  }
+};
+
+const create = async (cityData: Omit<ICitiesDetails, 'id'>): Promise<number | Error> => {
+  try {
+    const { data } = await Api.post<ICitiesDetails>('/cities', cityData);
+
+    if (data)
+      return data.id;
+
+    return new Error('Error creating a record.');
+  } catch (error) {
+    return new Error((error as { message: string }).message || 'Error creating a record.');
+  }
+};
+
+const updateById = async (id: number, cityData: ICitiesDetails): Promise<void | Error> => {
+  try {
+    await Api.put(`/cities/${id}`, cityData);
+  } catch (error) {
+    return new Error((error as { message: string }).message || 'Error updating record.');
+  }
+};
+
+const deleteById = async (id: number): Promise<void | Error> => {
+  try {
+    await Api.delete(`/cities/${id}`);
+  } catch (error) {
+    return new Error((error as { message: string }).message || 'Error deleting the record.');
+  }
+};
+
+export const CitiesService = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById
+};
