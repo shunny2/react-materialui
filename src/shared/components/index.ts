f2affc334a479304d8fac3c5670b767fec77f59c
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-08 10:17:14 -0300

    feat: added new register path

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './side-bar/SideBar';
 export * from './listing-toolbar/ListingToolbar';
 export * from './details-toolbar/DetailsToolbar';
 export * from './signIn/SignIn';
+export * from './register/Register';
