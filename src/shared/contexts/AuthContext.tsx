7327b04349668ba82a562d274eba79594f8b0112
Alexander Davis <alexander.davis.098@gmail.com>
2022-12-22 10:09:12 -0300

    refactor: replacing the use of local storage with cookies

--- src/shared/contexts/AuthContext.tsx
+++ src/shared/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 
 import { AuthService, IUserAuthProps } from '../services/api/auth/AuthService';
+import { getCookie, removeCookie, setCookie } from '../utils';
 
 interface IAuthContextData {
   isAuthenticated: boolean;
@@ -10,7 +11,8 @@ interface IAuthContextData {
 
 const AuthContext = createContext({} as IAuthContextData);
 
-const LOCAL_STORAGE_KEY__ACCESS_TOKEN = 'ACCESS_TOKEN';
+// const LOCAL_STORAGE_KEY__ACCESS_TOKEN = 'ACCESS_TOKEN';
+const COOKIE_KEY__ACCESS_TOKEN = 'ACCESS_TOKEN';
 
 interface IAuthProviderProps {
   children: ReactNode;
@@ -20,7 +22,8 @@ export const AuthProvider = ({ children }: IAuthProviderProps) => {
   const [accessToken, setAccessToken] = useState<string>();
 
   useEffect(() => {
-    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
+    // const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
+    const accessToken = getCookie(COOKIE_KEY__ACCESS_TOKEN);
 
     if (accessToken)
       setAccessToken(JSON.stringify(accessToken));
@@ -36,13 +39,15 @@ export const AuthProvider = ({ children }: IAuthProviderProps) => {
     if (result instanceof Error)
       return alert(result.message);
     
-    localStorage.setItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN, JSON.stringify(result.token));
+    // localStorage.setItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN, JSON.stringify(result.token));
+    setCookie(COOKIE_KEY__ACCESS_TOKEN, result.token);
     setAccessToken(result.token);
   }, []);
 
   const handleLogout = useCallback(async () => {
     await AuthService.logout();
-    localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
+    // localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
+    removeCookie(COOKIE_KEY__ACCESS_TOKEN);
     setAccessToken(undefined);
   }, []);
 
